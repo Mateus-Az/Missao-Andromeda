2a3d226c726d5e97096b5688f74aa0600f143206
Mateus-Az <mateo.azv3@gmail.com>
2023-09-19 16:27:26 -0300

    criação microserviço 01 to-do list : Missão Andrômeda

---
+++ M.A-To-do-List/src/main/java/br/mission/andromeda/M/A/Todo/List/Application.java
@@ -0,0 +1,13 @@
+package br.mission.andromeda.M.A.Todo.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ M.A-To-do-List/src/test/java/br/mission/andromeda/M/A/Todo/List/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mission.andromeda.M.A.Todo.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
